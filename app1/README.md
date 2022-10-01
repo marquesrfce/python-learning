e951f92fdc5457806f02f3b16cec617c3684c5ce
Rafael Marques <rfmapp@live.com>
2022-10-01 02:43:21 -0300

    First app requirements

---
+++ app1/README.md
@@ -0,0 +1,11 @@
+# Python App 1
+
+Write a python program that takes input from the user, apply changes to the given string and display it to the user. The sample file have the proper steps commented.
+
+
+#### The output should look like this:
+Enter your name: rafael marques
+The name as you typed: rafael marques
+The name with each word starting with upper case: Rafael Marques
+The name with each letter in upper case: RAFAEL MARQUES
+The name with each letter in lower case: rafael marques
---
+++ app1/app1-working-strings.py
@@ -0,0 +1,11 @@
+# Python program that takes input from the user, apply string methods and display it to the user.
+
+# 1 - Get input from the user asking for the full name.
+
+# 2 - Change the given string in order to make each word begin with a capital letter. Store the new string in a new variable.
+
+# 3 - Change the given string in order to make all letters upper case. Store the new string in a new variable.
+
+# 4 - Change the given string in order to make all letters lower case. Store the new string in a new variable.
+
+# 5 - Print the variables values to the user.
